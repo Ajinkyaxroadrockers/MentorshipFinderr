05d89e8f113b0b449492e5561e8308bde6677843
Ajinkya <ajinkyapw@gmail.com>
2026-05-14 23:07:22 +0530

    update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-this")
-
     database_url = os.environ.get("DATABASE_URL")
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
